03b047b9e1710ad5dd9fc6c0c76bd040669709d1
Jacek Tomaszewski <jacek@jtom.me>
2019-07-02 14:42:42 +0200

    Bootstrap Chat UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Chat } from './Chat';
 
 function App() {
-  return <div className="App">AppUnite - React workshop</div>;
+  return (
+    <div className="App">
+      <Chat />
+    </div>
+  );
 }
 
 export default App;
