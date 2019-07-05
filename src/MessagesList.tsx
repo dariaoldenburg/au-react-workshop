523880404f40d605a338759db75878075e94f75d
Daria Oldenburg <dariaoldenburg@gmail.com>
2019-07-06 01:33:07 +0200

    feat(MessagesList): add more info to message

--- src/MessagesList.tsx
+++ src/MessagesList.tsx
@@ -8,9 +8,22 @@ interface MessagesListProps {
 export function MessagesList(props: MessagesListProps) {
   return (
     <React.Fragment>
-      {props.messages.map(message => (
-        <div key={message.id || message.content}>{message.content}</div>
-      ))}
+      {props.messages.map(message => {
+        const date = new Date(message.createdAt);
+
+        return (
+          <div key={message.id || message.content}>
+            <strong>[{date.toLocaleTimeString()}]</strong>
+            {message.user && (
+              <span style={{ color: message.user.color }}>
+                {' '}
+                {message.user.name}:{' '}
+              </span>
+            )}
+            {message.content}
+          </div>
+        );
+      })}
     </React.Fragment>
   );
 }
