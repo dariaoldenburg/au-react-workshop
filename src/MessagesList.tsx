4afb0d3e0a7ef4340d2b6aad5bb2de45c3df332f
Daria Oldenburg <dariaoldenburg@gmail.com>
2019-07-05 23:11:57 +0200

    create(MessagesList)

---
+++ src/MessagesList.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Message } from './ChatAPI';
+
+interface MessagesListProps {
+  messages: Message[];
+}
+
+export function MessagesList(props: MessagesListProps) {
+  return (
+    <React.Fragment>
+      {props.messages.map(message => (
+        <div key={message.id || message.content}>{message.content}</div>
+      ))}
+    </React.Fragment>
+  );
+}
