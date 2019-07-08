b8a00443bfe8fe4b551da3a885bc747c6c75125c
Jacek Tomaszewski <jacek@jtom.me>
2019-07-08 14:49:04 +0200

    refactor: Improve Button tests a bit by moving creating div wrapper to jest beforeEach/afterEach callbacks

--- src/ui/Button.test.tsx
+++ src/ui/Button.test.tsx
@@ -3,27 +3,27 @@ import ReactDOM from 'react-dom';
 import { Button } from './Button';
 import { Simulate } from 'react-dom/test-utils';
 
+let div: HTMLDivElement;
+beforeEach(() => {
+  div = document.createElement('div');
+});
+afterEach(() => {
+  ReactDOM.unmountComponentAtNode(div);
+});
+
 it('renders the button element', () => {
-  const div = document.createElement('div');
   ReactDOM.render(<Button>text</Button>, div);
 
   const button = div.querySelector('button')!;
   expect(button).toBeTruthy();
-
   expect(button.textContent).toEqual('text');
-
-  ReactDOM.unmountComponentAtNode(div);
 });
 
 it('calls onClick on click', () => {
   const onClick = jest.fn();
-  const div = document.createElement('div');
   ReactDOM.render(<Button onClick={onClick} />, div);
 
   const button = div.querySelector('button')!;
-
   Simulate.click(button);
   expect(onClick).toHaveBeenCalledTimes(1);
-
-  ReactDOM.unmountComponentAtNode(div);
 });
