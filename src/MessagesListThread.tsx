b164f161c828c64364a924aa2386cee3f6ab5d1b
Jacek Tomaszewski <jacek@jtom.me>
2019-07-08 15:33:15 +0200

    In MessagesListThread, show submessages

--- src/MessagesListThread.tsx
+++ src/MessagesListThread.tsx
@@ -1,27 +1,82 @@
 import React from 'react';
 import { Message } from './ChatAPI';
+import { Button } from './ui/Button';
+
+const SubmessagesList = ({
+  messages,
+  extended,
+  onClick
+}: {
+  messages: Message[];
+  extended: boolean;
+  onClick(): void;
+}) => {
+  return (
+    <div style={{ marginLeft: 20 }}>
+      {extended &&
+        messages.map(message => (
+          <MessagesListThread key={message.id} message={message} />
+        ))}
+      <div>
+        <Button onClick={onClick}>
+          {!extended
+            ? `[show ${messages.length} other messages]`
+            : '[hide thread]'}
+        </Button>
+      </div>
+    </div>
+  );
+};
 
 interface MessagesListThreadProps {
   message: Message;
 }
 
-export function MessagesListThread({ message }: MessagesListThreadProps) {
-  const date = new Date(message.createdAt);
+interface MessagesListThreadState {
+  extended: boolean;
+}
 
-  return (
-    <div key={message.id || message.content}>
-      <strong>[{date.toLocaleTimeString()}]</strong>{' '}
-      {message.user && (
-        <span
-          style={{
-            color: message.user.color,
-            fontWeight: message.user.senior ? 'bold' : undefined
-          }}
-        >
-          {message.user.name}:{' '}
-        </span>
-      )}
-      {message.content}
-    </div>
-  );
+export class MessagesListThread extends React.PureComponent<
+  MessagesListThreadProps,
+  MessagesListThreadState
+> {
+  state = {
+    extended: false
+  };
+
+  handleShowMore = () => {
+    this.setState(state => ({
+      extended: !state.extended
+    }));
+  };
+
+  render() {
+    const { message } = this.props;
+    const { extended } = this.state;
+    const date = new Date(message.createdAt);
+
+    return (
+      <div key={message.id || message.content}>
+        <strong>[{date.toLocaleTimeString()}]</strong>{' '}
+        {message.user && (
+          <span
+            style={{
+              color: message.user.color,
+              fontWeight: message.user.senior ? 'bold' : undefined
+            }}
+          >
+            {message.user.name}:{' '}
+          </span>
+        )}
+        {message.content}
+        {message.submessages && message.submessages.length > 0 && (
+          <SubmessagesList
+            messages={message.submessages}
+            extended={extended}
+            onClick={this.handleShowMore}
+          />
+        )}
+      </div>
+    );
+  }
 }
